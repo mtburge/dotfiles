3d61c502f8f7cf7848e7e545332c6ccb38f03a30
Matt Burgess <mattburgess@macbook14.local>
2024-06-23 23:58:37 +0100

    add missing fish conf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,16 @@
 status --is-interactive; and begin
 
   # abbreviations
-  abbr --add --global -- dotfiles 'cd ~/.dotfiles'
+  abbr --add --global -- dotfiles "cd ~/.dotfiles"
   abbr --add --global -- g lazygit
 
   # aliases
-  alias aws-whoami 'aws sts get-caller-identity'
-  alias nah 'git reset --hard'
-  alias publicip 'dig +short myip.opendns.com @resolver1.opendns.com'
-  alias reload 'source ~/.config/fish/config.fish'
-
+  alias aws-whoami "aws sts get-caller-identity"
+  alias nah "git reset --hard"
+  alias publicip "dig +short myip.opendns.com @resolver1.opendns.com"
+  alias reload "source ~/.config/fish/config.fish"
+  alias flushdns "sudo killall -HUP mDNSResponder"
+  alias listening "sudo lsof -iTCP -sTCP:LISTEN -n -P"
 end
 
 
