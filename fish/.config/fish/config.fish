2afa1078a7b259c135d9c65f42113d6dffb6c476
Matt Burgess <hey@mtb.dev>
2024-06-24 01:18:25 +0100

    improve shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,24 @@
-status --is-interactive; and begin
+# Disable the fish greeting message
+set fish_greeting ""
 
-  # abbreviations
-  abbr --add --global -- dotfiles "cd ~/.dotfiles"
-  abbr --add --global -- g lazygit
+# Setup brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-  # aliases
-  alias aws-whoami "aws sts get-caller-identity"
-  alias nah "git reset --hard"
-  alias publicip "dig +short myip.opendns.com @resolver1.opendns.com"
-  alias reload "source ~/.config/fish/config.fish"
-  alias flushdns "sudo killall -HUP mDNSResponder"
-  alias listening "sudo lsof -iTCP -sTCP:LISTEN -n -P"
+# abbreviations
+abbr --global -- dotfiles "cd ~/.dotfiles"
+abbr --global -- g lazygit
+
+# aliases
+alias aws-whoami "aws sts get-caller-identity | jq"
+alias nah "git reset --hard"
+alias publicip "dig +short myip.opendns.com @resolver1.opendns.com"
+alias reload "source ~/.config/fish/config.fish"
+alias flushdns "sudo killall -HUP mDNSResponder"
+alias listening "sudo lsof -iTCP -sTCP:LISTEN -n -P"
+
+# `ls` → `eza` abbreviation
+if type -q eza
+  abbr --global ll 'eza --long --classify --all --header --git --no-user --tree --level 1'
 end
 
-
+zoxide init fish | source
