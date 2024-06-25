d1e84bc83d7d111021a558a0a6d9d776bbad09f5
Matt Burgess <hey@mtb.dev>
2024-06-25 13:45:50 +0100

    use tree with user

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --description 'Use eza instead of ls -lah'
-  eza --long --classify --all --header --git --no-user --icons --level 1 $argv
+  eza --long --classify --all --header --git --icons --tree --level 1 $argv
 end
