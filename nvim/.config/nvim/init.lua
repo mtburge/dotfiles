0d98d95df14702735aebca216b6442dcac72bc49
Matt Burgess <hey@mtb.dev>
2024-06-27 21:26:19 +0100

    better window movement bindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,10 +38,10 @@ vim.keymap.set("n", "<S-Tab>", "<cmd>:bprev<CR>", { desc = "<Shift+Tab> to previ
 vim.keymap.set("n", "<C-N>", "<cmd>:enew<CR>", { desc = "<Ctrl+N> to open a new buffer" })
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader><left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<leader><right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<leader><down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<leader><up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
