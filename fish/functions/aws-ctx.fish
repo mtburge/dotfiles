860528194572c3e201906a711d91a3d4580bdb68
Matt Burgess <hey@mtb.dev>
2025-01-25 15:31:18 +0000

    setup autologin for sso profiles

--- fish/functions/aws-ctx.fish
+++ fish/functions/aws-ctx.fish
@@ -1,4 +1,10 @@
 function aws-ctx --description 'Switch AWS profile'
   export AWS_PROFILE="$(aws configure list-profiles | fzf)"
+
+  # for SSO profiles, refresh the session
+  if test -n "$(aws configure get profile.$AWS_PROFILE.sso_session)"
+    aws sso login --profile "$AWS_PROFILE"
+  end
+
   echo "Switched to $AWS_PROFILE profile."
 end
