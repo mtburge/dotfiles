1c8cbcaa7c63c328a206f9e470dabed25239c13a
Matt Burgess <hey@mtb.dev>
2025-11-02 13:42:13 +0000

    open nvim and git by default

--- tmux/workspace.sh
+++ tmux/workspace.sh
@@ -5,8 +5,8 @@ SESSION_NAME="${1:-workspace}"
 tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -d -s $SESSION_NAME -n nvim
-  tmux new-window -t $SESSION_NAME:2 -n git
+  tmux new-session -d -s $SESSION_NAME -n nvim 'nvim'
+  tmux new-window -t $SESSION_NAME:2 -n git 'lazygit'
   tmux new-window -t $SESSION_NAME:3 -n dev
   tmux new-window -t $SESSION_NAME:4 -n console
 fi
