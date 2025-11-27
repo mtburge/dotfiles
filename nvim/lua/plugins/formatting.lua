c4f1af46781978993baf681d955b1fba113d372f
Matt Burgess <hey@mtb.dev>
2025-11-27 12:09:44 +0000

    improve formatting perf; reliability

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -2,26 +2,29 @@ return {
 	{ "nmac427/guess-indent.nvim" },
 	{
 		"stevearc/conform.nvim",
-		lazy = false,
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
 		opts = {
 			notify_on_error = false,
 			format_on_save = {
-				timeout_ms = 1000,
-				lsp_fallback = true,
+				timeout_ms = 2500,
+				lsp_fallback = "fallback",
+			},
+			formatters = {
+				ruff_fix = {
+					stdin = true,
+					args = {
+						"check",
+						"--fix",
+						"--stdin-filename",
+						"$FILENAME",
+						"-",
+					},
+				},
 			},
 			formatters_by_ft = {
+				["_"] = { "trim_whitespace" },
+				go = { "goimports", "gofmt" },
 				lua = { "stylua" },
-				python = { "ruff_format", "ruff_fix" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				javascript = { "eslint_d", "prettier" },
 				javascriptreact = { "eslint_d", "prettier" },
 				typescript = { "eslint_d", "prettier" },
